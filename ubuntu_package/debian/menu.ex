5a01176a2e8959ce13f3bcb091ffb97ca3e055ee
Marcin Szczodrak <msz@cs.columbia.edu>
2013-08-17 20:40:40 -0400

    t;

--- ubuntu_package/debian/menu.ex
+++
@@ -1,2 +0,0 @@
-?package(sfc):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
-  title="sfc" command="/usr/bin/sfc"
--- ubuntu_package/sfc-0.79/debian/sfc.cron.d.ex
+++
@@ -1,4 +0,0 @@
-#
-# Regular cron jobs for the sfc package
-#
-0 4	* * *	root	[ -x /usr/bin/sfc_maintenance ] && /usr/bin/sfc_maintenance
