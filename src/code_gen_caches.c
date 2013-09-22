d177563a153e86fd6e85bd3c233877245d3a3dd2
Marcin <msz@cs.columbia.edu>
2013-09-22 00:15:55 -0400

    up

--- src/code_gen_caches.c
+++ src/code_gen_caches.c
@@ -162,7 +162,7 @@ void generateCaches(int event_counter, int policy_counter) {
 
 
 	/* module_t to event_id */
-	fprintf(fp, "uint8_t event_module_conf[NUMBER_OF_EVENTS][3] = {\n");
+	fprintf(fp, "struct event_module_conf event_module_conf[NUMBER_OF_EVENTS] = {\n");
 	fprintf(fp, "\t/* [event_id, module_id, conf_id */\n");
 	for ( i = 0; i < conf_id_counter; i++ ) {
 		if (conftab[i].conf->id->type == TYPE_PROCESS_EVENT) {
