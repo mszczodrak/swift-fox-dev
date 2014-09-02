1af9f4de59cd1903bcaf1f92cf53eae0e231ec04
Marcin K Szczodrak <msz@cs.columbia.edu>
2014-09-02 15:21:16 -0400

    update

--- src/code_gen_globals.c
+++ src/code_gen_globals.c
@@ -415,21 +415,21 @@ void setVariableValue(struct variable *sh, struct confnode* current_process_gen,
 
 	if ((generate_globals && sh->used == 1)) {
 		if (sh->length > 1) {
-			fprintf(fp, "\t.%-30s = {%Lf},\t/* %d */\n", sh->name,
+			fprintf(fp, "\t.%-50s = {%Lf},\t/* %d */\n", sh->name,
 						sh->value, sh->offset);
 		} else {
-			fprintf(fp, "\t.%-30s = %Lf,\t/* %d */\n", sh->name,
+			fprintf(fp, "\t.%-50s = %Lf,\t/* %d */\n", sh->name,
 						sh->value, sh->offset);
 		}
 	}
 
 	if (sh->class_type == TYPE_VARIABLE_LOCAL) {
 		if (sh->length > 1) {
-			fprintf(fp, "\t.%s_%s_%-30s = {%Lf},\t/* %d */\n",
+			fprintf(fp, "\t.%s_%s_%-50s = {%Lf},\t/* %d */\n",
 						current_process_gen->name, current_module_gen->name,
 						sh->name, sh->value, sh->offset);
 		} else {
-			fprintf(fp, "\t.%s_%s_%-30s = %Lf,\t/* %d */\n",
+			fprintf(fp, "\t.%s_%s_%-50s = %Lf,\t/* %d */\n",
 						current_process_gen->name, current_module_gen->name,
 						sh->name, sh->value, sh->offset);
 		}
@@ -459,10 +459,10 @@ void setProcessLookupTable(struct confnode* c) {
 	for( i = 0; i < conf_id_counter; i++ ) {
 		for (vc = 0, mvar = conftab[i].conf->app->variables; vc < conftab[i].conf->app_var_num && mvar != NULL; vc++) {
 			if (mvar->var->class_type == TYPE_VARIABLE_GLOBAL) {
-				fprintf(fp, "\t{ %-30s, \t&(fennec_global_data.%s)\t},\n",
+				fprintf(fp, "\t{ %-30s, \t&(fennec_global_data.%-30s)\t},\n",
 						mvar->var->cap_name, mvar->var->gname);
 			} else {
-				fprintf(fp, "\t{ %-30s, \t&(fennec_local_data.%s_%s_%s)\t},\n",
+				fprintf(fp, "\t{ %-30s, \t&(fennec_local_data.%s_%s_%-30s)\t},\n",
 						mvar->var->cap_name, conftab[i].conf->name,
 						conftab[i].conf->app->name, mvar->var->name);
 			}
@@ -474,10 +474,10 @@ void setProcessLookupTable(struct confnode* c) {
 
 		for (vc = 0, mvar = conftab[i].conf->net->variables; vc < conftab[i].conf->net_var_num && mvar != NULL; vc++) {
 			if (mvar->var->class_type == TYPE_VARIABLE_GLOBAL) {
-				fprintf(fp, "\t{ %-30s, \t&(fennec_global_data.%s)\t},\n",
+				fprintf(fp, "\t{ %-30s, \t&(fennec_global_data.%-30s)\t},\n",
 						mvar->var->cap_name, mvar->var->gname);
 			} else {
-				fprintf(fp, "\t{ %-30s, \t&(fennec_local_data.%s_%s_%s)\t},\n",
+				fprintf(fp, "\t{ %-30s, \t&(fennec_local_data.%s_%s_%-30s)\t},\n",
 						mvar->var->cap_name, conftab[i].conf->name,
 						conftab[i].conf->net->name, mvar->var->name);
 			}
@@ -489,10 +489,10 @@ void setProcessLookupTable(struct confnode* c) {
 
 		for (vc = 0, mvar = conftab[i].conf->am->variables; vc < conftab[i].conf->am_var_num && mvar != NULL; vc++) {
 			if (mvar->var->class_type == TYPE_VARIABLE_GLOBAL) {
-				fprintf(fp, "\t{ %-30s, \t&(fennec_global_data..%s)\t},\n",
+				fprintf(fp, "\t{ %-30s, \t&(fennec_global_data.%-30s)\t},\n",
 						mvar->var->cap_name, mvar->var->gname);
 			} else {
-				fprintf(fp, "\t{ %-30s, \t&(fennec_local_data.%s_%s_%s)\t},\n",
+				fprintf(fp, "\t{ %-30s, \t&(fennec_local_data.%s_%s_%-30s)\t},\n",
 						mvar->var->cap_name, conftab[i].conf->name,
 						conftab[i].conf->am->name, mvar->var->name);
 			}
